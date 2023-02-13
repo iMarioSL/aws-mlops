9440a93e0ab547f8138bb941770d641a2909cdf5
Arturo <soberonarturo@gmail.com>
2023-02-12 20:55:01 -0600

    Removed redundancies

--- code/02-predict.py
+++ code/02-predict.py
@@ -1,39 +1,34 @@
 # Imports
 from pickle import load
-from json import loads
 
 # Load model
 clf = load(
-    file=open('../results/clf.sav', 'rb')
+    file=open('./results/clf.sav', 'rb')
 )
 
 # Class 0
-j0 = """
-{
+j0 = {
     "Mean concave poinst": 0.1,
     "Worst radius": 1.42,
     "Worst texture": 4.7,
     "Worst area": 14.93,
     "Worst concave points": 0.2
 }
-"""
 
 # Class 1
-j1 = """
-{
+j1 = {
     "Mean concave poinst": 0.19,
     "Worst radius": 33.13,
     "Worst texture": 23.58,
     "Worst area": 3234.0,
     "Worst concave points": 0.28
 }
-"""
 
 # Define function
 def make_pred(request=None):
 
     # Load request
-    d = loads(request)
+    d = request
     x = [d[k] for k in d]
 
     # Make prediction
