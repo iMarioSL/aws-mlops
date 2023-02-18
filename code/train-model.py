c1c31cdf211de1a0c5d5f0edbe87cbd5df4d446c
Arturo <soberonarturo@gmail.com>
2023-02-17 20:27:06 -0600

    Init train py file

---
+++ code/train-model.py
@@ -0,0 +1,8 @@
+################################
+# Training a simple classifier #
+################################
+
+# This notebook will fit a gradient boosted ensemble of trees to the infamous
+# 1995 breast cancer dataset. The goal is to produce a model that can
+# somewhat-accurately predict if a patient has breast cancer or not.
+# We will then export the fitted model and deploy it using AWS Lambda.
